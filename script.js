6ead58b396586969d1ed23fb8a1f3d358502f391
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-06-27 12:55:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	for(let i=0; i<key.length; i++) // i iterate key times
+	for(let k in sampleObject) // i iterate key times
 		if(i===key){
 			return true;
 		}
@@ -10,5 +10,6 @@ function hasKey(key) {
 }
 
 // Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+//const key = prompt("Enter Key.");
+//
+//alert(hasKey(key));
